761c21da7e36d3ea1ee9aef5113c0217f93ecaaa
Ted Michael Celis <krazete@gmail.com>
2018-10-30 19:32:27 -0700

    wrote rough draft functions to generate website data

--- assets2js.py
+++ assets2js.py
@@ -8,11 +8,6 @@ from collections import namedtuple
 # 3. Dump MonoBehaviours into monolith.
 # 4. Using character identifiers, extract character info from monolith.
 
-unvisited = []
-
-monolith_char_keys = []
-data = {}
-
 Character = namedtuple('Character', ('fighter', 'tier', 'variant_id', 'variant',))
 
 # Loader
@@ -39,18 +34,18 @@ def build_corpus():
         corpus.setdefault(stem, content)
     return corpus
 
-def get_corpus_characters(corpus):
-    'Extract character identifiers from corpus.'
+def get_corpus_character_keys(corpus):
+    'Extract character identifiers {corpus key: Character} from corpus.'
     name_pattern = re.compile('Char_([A-Za-z]+)_(\w)_V(\d+)_Name')
-    characters = {}
+    corp_char_keys = {}
     for key in corpus['en']:
         matches = re.findall(name_pattern, key)
         for match in matches: # should loop once
             fighter, tier, variant_id = match
             variant = corpus['en'][key]
             character = Character(fighter, tier, variant_id, variant)
-            characters.setdefault(key, character)
-    return characters
+            corp_char_keys.setdefault(key, character)
+    return corp_char_keys
 
 # Build Monolith
 
@@ -65,13 +60,15 @@ def build_monolith():
             monolith.setdefault(key, content)
     return monolith
 
-def get_mono_char_keys(characters):
-    mono_char_keys = []
+def get_monolith_character_keys(corp_char_keys):
+    'Extract character objects {monolith key: corpus key} from monolith.'
+    mono_char_keys = {}
     for key in monolith:
         mono = monolith[key]
         if 'displayVariantName' in mono:
-            if mono['displayVariantName'] in characters:
-                mono_char_keys.append(key)
+            if mono['displayVariantName'] in corp_char_keys:
+                corp_char_key = mono['displayVariantName']
+                mono_char_keys.setdefault(key, corp_char_key)
     return mono_char_keys
 
 # Study Objects
@@ -83,14 +80,14 @@ def study_dict(dictionary):
 
 def gather_mono_refs(obj, ignored_keys=set(), is_root=True):
     'Replace references with the objects they point to (for further study).'
-    global visited_monolith_keys
+    global visited_mono_keys
     if is_root:
-        visited_monolith_keys = set()
+        visited_mono_keys = set()
     if isinstance(obj, dict):
         if 'm_PathID' in obj:
             key = str(obj['m_PathID'])
-            if key not in visited_monolith_keys:
-                visited_monolith_keys.add(key)
+            if key not in visited_mono_keys:
+                visited_mono_keys.add(key)
                 try:
                     value = gather_mono_refs(monolith[key], ignored_keys, False)
                 except KeyError:
@@ -114,28 +111,88 @@ def gather_mono_refs(obj, ignored_keys=set(), is_root=True):
     else:
         return obj
 
-# def monoref(ref, objkey):
-#     'Return the monolith object referenced to by the reference object.'
-#     key = str(ref['m_PathID'])
-#     return monolith[key][objkey]
+# Build Data
+
+def monoref(ref):
+    'Return the monolith object referenced to by the reference object.'
+    key = str(ref['m_PathID'])
+    return monolith[key]
+
+def build_datum(corp, mono):
+    'Generate data for the database to be used on the website.'
+    base = monoref(mono['baseCharacter'], )
+    f_key = base['humanReadableGuid']
+    v_key = mono['humanReadableGuid']
+    if f_key == '' or v_key == '': # for bronze parasoul and peacock, dunno why
+        return None, None, None, None
+    f_value = {
+        'dataName': base['dataName'],
+        'name': corp[base['displayName']],
+        'marquee': mono['superAbility'],
+        'superDeath': base['superDeathTexture']['resourcePath']
+    }
+    v_value = {
+        'base': f_key,
+        'name': corp[mono['displayVariantName']],
+        'quote': corp[mono['variantQuote']],
+        'tier': mono['initialTier'],
+        'element': mono['elementAffiliation'],
+        'baseStats': mono['baseScaledValuesByTier'],
+        'signature': {},
+        'tint': mono['tintColor'],
+        'enabled': 1
+    }
+    return f_key, f_value, v_key, v_value
+#     if fighter not in fighterdata:
+#         build_fighter_datum(fighter)
+#     signature_ref = mono['signatureAbility']
+#     value.setdefault('signature', {})
+#     value['signature'].setdefault('name', corpus[language][monoref(signature_ref, 'title')])
+#     value['signature'].setdefault('ability', [])
+#     for iteration in monoref(signature_ref, 'tiers'):
+#         ability =
+#         value['signature']['ability'].append()
+#             'ability': [
+#                 {
+#                     'name': corpus[language][monoref(signature_ref, 'description')],
+#                     'iterations': [monolith[str(iteration['m_PathID'])]['modifierSets']
+#                         for iteration in monoref(signature_ref, 'tiers')
+#                     ]
+#                 }
+#             ]
+#         } for signature in monoref(signature_ref, 'features')
+#     ])
 #
-# def build_data():
-#     'Generate database to be used on the website.'
-#     for char_key in monolith_char_keys:
-#         mono = monolith[char_key]
-#         for language in corpus:
-#             key, value = build_datum(mono, language)
-#             data.setdefault(language, {})
-#             data[language].setdefault(key, value)
-#     data_names = [data['en'][key]['name'] for key in data['en']]
-#     for key in unvisited:
-#         for language in data:
-#             value = {
-#                 'name': corpus[language][key],
-#                 'enabled': 0
-#             }
-#             data[language].setdefault(key, value)
+#     value.setdefault('super', mono['superAbility'])
 #
+
+def build_disabled_datum(corp):
+    return 0, 0, 0, 0
+
+def build_data(corpus, monolith, corp_char_keys, mono_char_keys):
+    'Generate database to be used on the website.'
+    fighters = {}
+    variants = {}
+    for mono_char_key in mono_char_keys:
+        corp_char_key = mono_char_keys[mono_char_key]
+        mono = monolith[mono_char_key]
+        for language in corpus:
+            corp = corpus[language]
+            f_key, f_value, v_key, v_value = build_datum(corp, mono)
+            fighters.setdefault(language, {})
+            fighters[language].setdefault(f_key, f_value)
+            variants.setdefault(language, {})
+            variants[language].setdefault(v_key, v_value)
+    for corp_char_key in corp_char_keys:
+        corp = corp_char_keys[corp_char_key]
+        for language in corpus:
+            f_key, f_value, v_key, v_value = build_disabled_datum(corp)
+            fighters.setdefault(language, {})
+            fighters[language].setdefault(f_key, f_value)
+            variants.setdefault(language, {})
+            variants[language].setdefault(v_key, v_value)
+    return fighters, variants
+
 # fighterdata = {
 #     'filia': {
 #         'name': 'Filia',
@@ -181,61 +238,27 @@ def gather_mono_refs(obj, ignored_keys=set(), is_root=True):
 #         'name': 'Heavy Metal'
 #     }
 # }
-#
-# def build_datum(mono, language):
-#     'Generate data for the database to be used on the website.'
-#     corp = corpus[language]
-#     key = mono['humanReadableGuid']
-#     value = {
-#         'name': corp[mono['displayVariantName']]),
-#         'quote': corp[mono['variantQuote']]),
-#         'tier': mono['initialTier']),
-#         'element': mono['elementAffiliation']),
-#         'baseStats': mono['baseScaledValuesByTier']),
-#         'tint': mono['tintColor']),
-#         'enabled': 1
-#     }
-#     if fighter not in fighterdata:
-#         build_fighter_datum(fighter)
-#     signature_ref = mono['signatureAbility']
-#     value.setdefault('signature', {})
-#     value['signature'].setdefault('name', corpus[language][monoref(signature_ref, 'title')])
-#     value['signature'].setdefault('ability', [])
-#     for iteration in monoref(signature_ref, 'tiers'):
-#         ability =
-#         value['signature']['ability'].append()
-#             'ability': [
-#                 {
-#                     'name': corpus[language][monoref(signature_ref, 'description')],
-#                     'iterations': [monolith[str(iteration['m_PathID'])]['modifierSets']
-#                         for iteration in monoref(signature_ref, 'tiers')
-#                     ]
-#                 }
-#             ]
-#         } for signature in monoref(signature_ref, 'features')
-#     ])
-#
-#     value.setdefault('super', mono['superAbility'])
-#
-#     return key, value
 
 if __name__ == '__main__':
     corpus = build_corpus()
     monolith = build_monolith()
 
-    characters = get_corpus_characters(corpus)
-    mono_char_keys = get_mono_char_keys(characters)
+    corp_char_keys = get_corpus_character_keys(corpus)
+    mono_char_keys = get_monolith_character_keys(corp_char_keys)
 
-    # study_dict(monolith[mono_char_keys[59]]) # check 37, 40, ...
+    {key for key in mono_char_keys if 'Filia_B' in mono_char_keys[key]}
+    study_dict(monolith['15576'])
 
-    ignored_keys = ['skillTree', 'homeStage', 'blockbusters', 'specialMoves', 'evoCelebration_HitAction']
-    bHDay_study = gather_mono_refs(monolith[mono_char_keys[59]], ignored_keys)
+    ignored_keys = ['skillTree', 'homeStage', 'blockbusters', 'specialMoves', 'evoCelebration_HitAction', 'signatureAbility']
+    bHDay_study = gather_mono_refs(monolith['15576'], ignored_keys)
     with open('sgm_exports/study.json', 'w') as fp:
         json.dump(bHDay_study, fp, indent=4, separators=(',', ': '))
 
-    # build_data()
+    fighters, variants = build_data(corpus, monolith, corp_char_keys, mono_char_keys)
 
-    # with open('official_data.js', 'w') as fp:
-    #     json.dump(data, fp)
+    with open('fighter_data.json', 'w') as fp:
+        json.dump(fighters, fp)
+    with open('variant_data.json', 'w') as fp:
+        json.dump(variants, fp)
 
 # data['en']['bHDay']
