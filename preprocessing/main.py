0704efcb5096a065e5a2cec00d33433c6aee70ba
Ted Michael Celis <krazete@gmail.com>
2018-11-05 16:03:54 -0800

    split up build_datum and generated data folder

--- preprocessing/main.py
+++ preprocessing/main.py
@@ -44,6 +44,14 @@ def compile_mono(obj, is_root=True):
     else:
         return obj
 
+def study_sample(monolith, mono_char_keys):
+    from random import sample
+    for key in sample(mono_char_keys, 1):
+        mono = monolith[key]
+        study_dictionary(mono)
+        monostudy = compile_mono(mono)
+        file.save(monostudy, 'preprocessing/study.json')
+
 # Get Character Keys
 
 def get_monolith_character_keys(monolith):
@@ -51,7 +59,7 @@ def get_monolith_character_keys(monolith):
     character_keys = set()
     for key in monolith:
         mono = monolith[key]
-        if 'humanReadableGuid' in mono and 'baseCharacter' in mono:
+        if 'baseCharacter' in mono:
             character_keys.add(key)
     return character_keys
 
@@ -86,50 +94,68 @@ def build_data(monolith, mono_char_keys):
         corpus_keys.add(key)
         return key
 
-    def build_datum(mono):
-        'Generate data for the database to be used on the website.'
+    def build_fighter(mono):
         base = monoref(mono['baseCharacter'])
         f_key = base['humanReadableGuid']
-        v_key = mono['humanReadableGuid']
-        if f_key == '' or v_key == '': # for bronze parasoul and peacock, dunno why
+        if f_key in fighters: # skip copies
             return
         f_value = {
-            # 'role': record(base['roleDescription']),
-            # 'dataName': base['dataName'],
-            # 'superDeath': base['superDeathTexture']['resourcePath'],
-            # 'portrait': monoref(base['hudPortraitPalettizedImage'])['resourcePath'],
+            'dataName': base['dataName'], # TODO: maybe delete
             'name': record(base['displayName']),
+            'role': record(base['roleDescription']), # TODO: maybe delete
+            # 'loading': monoref(base['characterAbility'])['resourcePath'], # TODO: maybe delete
+            'death': base['superDeathTexture']['resourcePath'], # TODO: maybe delete
+            # 'portrait': monoref(base['hudPortraitPalettizedImage'])['resourcePath'], # TODO: maybe delete
             'voice': {
                 'en': base['englishVoArtist'],
                 'ja': base['japaneseVoArtist']
             },
-            'ability': {
-                'name': record(monoref(base['characterAbility'])['title']),
-                'description': record(monoref(base['characterAbility'])['description'])
-                # 'charge': record(monoref(base['characterAbility'])['chargeTimeInFrames'])
-            },
-            'marquee': mono['superAbility'],
-            'blockbusters': [monoref(x) for x in base['blockbusters']],
-            'specials': {}
+            # 'blockbusters': [monoref(x) for x in base['blockbusters']], # TODO: add this in
+            # 'specialmoves': {}, # TODO: add this in
+            'characterability': monoref(base['characterAbility']),
+            # 'marquee': {
+            #     'title': monoref(mono['superAbility'])['title'],
+            #     'features': [monoref(feature) for feature in monoref(mono['superAbility'])['features']] # TODO: fix this
+            # }
         }
+        fighters.setdefault(f_key, f_value)
+
+    def build_variant(mono):
+        base = monoref(mono['baseCharacter'])
+        f_key = base['humanReadableGuid']
+        hrid = mono['humanReadableGuid']
+        if hrid == '':
+            v_key = mono['guid']
+        elif hrid == 'dummy':
+            v_key = f_key + '_dummy'
+        else:
+            v_key = hrid
+        if v_key in variants: # skip copies (except '' or 'dummy')
+            return
         v_value = {
             'base': f_key,
             'name': record(mono['displayVariantName']),
+            'role': record(mono['variantDescription']), # TODO: maybe delete
             'quote': record(mono['variantQuote']),
             'tier': mono['initialTier'],
             'element': mono['elementAffiliation'],
             'baseStats': mono['baseScaledValuesByTier'],
-            'tint': mono['tintColor'], # not sure how to use this yet
-            'enabled': 1
+            'palette': mono['paletteIndex'], # TODO: maybe delete
+            'paletteURL': monoref(mono['cardPortraitPalettizedImage'])['dynamicSprite']['resourcePath'], # TODO: maybe delete
+            'tint': mono['tintColor'], # TODO: maybe delete
+            # 'signature': {
+            #     'title': monoref(mono['signatureAbility'])['title'],
+            #     'features': [monoref(feature) for feature in monoref(mono['signatureAbility'])['features']] # TODO: fix this
+            # },
+            'enabled': v_key == hrid
         }
-        fighters.setdefault(f_key, f_value)
         variants.setdefault(v_key, v_value)
 
     for key in mono_char_keys:
         mono = monolith[key]
-        if 'humanReadableGuid' in mono and 'baseCharacter' in mono:
-            # print('{:3}{:16}{}'.format(monoref(mono['baseCharacter'])['humanReadableGuid'], mono['humanReadableGuid'], mono['baseScaledValuesByTier']))
-            build_datum(mono)
+        build_fighter(mono)
+        build_variant(mono)
+
     return fighters, variants, corpus_keys
 
 if __name__ == '__main__':
@@ -139,16 +165,12 @@ if __name__ == '__main__':
     mono_char_keys = get_monolith_character_keys(monolith)
     corp_char_keys = get_corpus_character_keys(corpus)
 
-    # from random import sample
-    # for key in sample(mono_char_keys, 1):
-    #     mono = monolith[key]
-    #     study_dictionary(mono)
-    #     monostudy = compile_mono(mono)
-    #     file.save(monostudy, 'preprocessing/data/study.json')
+    study_sample(monolith, mono_char_keys)
 
     fighters, variants, corpus_keys = build_data(monolith, mono_char_keys)
 
     for language in corpus:
-        file.save(corpus[language], 'preprocessing/data/{}.json'.format(language))
-    file.save(fighters, 'preprocessing/data/fighter_data.json')
-    file.save(variants, 'preprocessing/data/variant_data.json')
+        primcorpus = {key: corpus[language][key] for key in corpus_keys if key in corpus[language]}
+        file.save(primcorpus, 'data/{}.json'.format(language))
+    file.save(fighters, 'data/fighter_data.json')
+    file.save(variants, 'data/variant_data.json')
