a3e87192fcfe46eda5f0a1bff6e88bae7f6e5523
Ted Michael Celis <krazete@gmail.com>
2018-11-10 11:54:42 -0800

    organized index.js, added animation to numbers

--- index.js
+++ index.js
@@ -1,41 +1,5 @@
 var corpus, fighters, variants;
-
 var collection, order, sa, ma, red;
-
-function loadLanguage() {
-    return window.localStorage.getItem("language") || "en";
-}
-
-function saveLanguage(language) {
-    window.localStorage.setItem("language", language);
-}
-
-function resetCollection() {
-    collection.innerHTML = "";
-}
-
-function changeLanguage(language) {
-    saveLanguage(language);
-    resetCollection();
-    initialize();
-}
-
-function load(path) {
-    function request(resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", path, true);
-        xhr.onload = function() {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                resolve(JSON.parse(this.response));
-    		}
-    	};
-        xhr.onerror = function() {
-            reject(new Error("Could not load '" + path + "'."));
-        };
-        xhr.send();
-    }
-    return new Promise(request);
-}
 
 function initialize() {
     collection = document.getElementById("collection");
@@ -62,57 +26,39 @@ function initialize() {
     ]).then(callback);
 }
 
-function newString(string, className) {
-    var title = document.createElement("div");
-    title.className = className;
-    title.innerHTML = string;
-    return title;
+function changeLanguage(language) {
+    saveLanguage(language);
+    resetCollection();
+    initialize();
 }
 
-function formatDescription(feature, i, id) {
-    return format(corpus[feature.description], feature.tiers.slice(i)[0], id);
+function loadLanguage() {
+    return window.localStorage.getItem("language") || "en";
 }
 
-function sort(method) {
-    var sorted = Object.keys(variants).sort(method);
-    for (var id of sorted) {
-        var card = document.getElementById(id);
-        collection.appendChild(card);
+function saveLanguage(language) {
+    window.localStorage.setItem("language", language);
+}
+
+function resetCollection() {
+    collection.innerHTML = "";
+}
+
+function load(path) {
+    function request(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", path, true);
+        xhr.onload = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                resolve(JSON.parse(this.response));
+    		}
+    	};
+        xhr.onerror = function() {
+            reject(new Error("Could not load '" + path + "'."));
+        };
+        xhr.send();
     }
-    order = method;
-}
-
-function fs(f) {
-    return (f.lifebar / 6 + f.attack) * 7 / 10;
-}
-
-function byFS(a, b) {
-    var varA = variants[a];
-    var varB = variants[b];
-    var A = varA.baseStats[varA.baseStats.length - 1];
-    var B = varB.baseStats[varB.baseStats.length - 1];
-    return fs(B) - fs(A);
-}
-function byHP(a, b) {
-    var varA = variants[a];
-    var varB = variants[b];
-    var A = varA.baseStats[varA.baseStats.length - 1];
-    var B = varB.baseStats[varB.baseStats.length - 1];
-    return B.lifebar - A.lifebar;
-}
-function byAttack(a, b) {
-    var varA = variants[a];
-    var varB = variants[b];
-    var A = varA.baseStats[varA.baseStats.length - 1];
-    var B = varB.baseStats[varB.baseStats.length - 1];
-    return B.attack - A.attack;
-}
-function byAlpha(a, b) {
-    var varA = variants[a];
-    var varB = variants[b];
-    var A = fighters[varA.base].name + varA.name;
-    var B = fighters[varB.base].name + varB.name;
-    return A > B ? 1 : A < B ? -1 : 0;
+    return new Promise(request);
 }
 
 function init(sa, ma, sig_only) {
@@ -133,33 +79,33 @@ function init(sa, ma, sig_only) {
             var loading = loadingBack.appendChild(document.createElement("img"));
             loading.className = "loading";
             loading.src = "data/image/" + fighters[variant.base].loading + ".png";
-            div.appendChild(newString(corpus[fighters[variant.base].name], "fighter"));
-            div.appendChild(newString(corpus[variant.name], "variant"));
-            div.appendChild(newString(corpus[variant.quote], "quote"));
+            div.appendChild(newStringDiv(corpus[fighters[variant.base].name], "fighter"));
+            div.appendChild(newStringDiv(corpus[variant.name], "variant"));
+            div.appendChild(newStringDiv(corpus[variant.quote], "quote"));
             if (!sig_only) {
-                div.appendChild(newString(corpus[fighters[variant.base].characterability.title], "ability"));
-                div.appendChild(newString(corpus[fighters[variant.base].characterability.description], "description"));
+                div.appendChild(newStringDiv(corpus[fighters[variant.base].characterability.title], "ability"));
+                div.appendChild(newStringDiv(corpus[fighters[variant.base].characterability.description], "description"));
             }
-            div.appendChild(newString(corpus[variant.signature.title], "ability"));
+            div.appendChild(newStringDiv(corpus[variant.signature.title], "ability"));
             for (var feature of variant.signature.features) {
-                div.appendChild(newString(formatDescription(feature, sa, "sa"), "description"));
+                div.appendChild(newStringDiv(formatDescription(feature, sa), "description"));
             }
             if (!sig_only) {
-                div.appendChild(newString(corpus[fighters[variant.base].marquee.title], "ability"));
+                div.appendChild(newStringDiv(corpus[fighters[variant.base].marquee.title], "ability"));
                 for (var feature of fighters[variant.base].marquee.features) {
-                    div.appendChild(newString([
-                        "<b>" + corpus[feature.title] + "</b>", formatDescription(feature, ma, "ma")
+                    div.appendChild(newStringDiv([
+                        "<b>" + corpus[feature.title] + "</b>", formatDescription(feature, ma)
                     ].join(" - "), "description"));
                 }
             }
             div.innerHTML += "<br>";
-            div.appendChild(newString(["Bronze", "Silver", "Gold", "Diamond"][variant.tier], "ability"));
-            div.appendChild(newString(["Neutral", "Fire", "Water", "Wind", "Dark", "Light"][variant.element], "ability"));
+            div.appendChild(newStringDiv(["Bronze", "Silver", "Gold", "Diamond"][variant.tier], "ability"));
+            div.appendChild(newStringDiv(["Neutral", "Fire", "Water", "Wind", "Dark", "Light"][variant.element], "ability"));
             div.innerHTML += "<br>";
-            div.appendChild(newString("Base Stats", "ability"));
+            div.appendChild(newStringDiv("Base Stats", "ability"));
             var i = variant.tier;
             for (var stat of variant.baseStats) {
-                div.appendChild(newString("[" + ["Bronze", "Silver", "Gold", "Diamond"][i] + "] HP: " + stat.lifebar + " / ATK: " + stat.attack, "description"));
+                div.appendChild(newStringDiv("[" + ["Bronze", "Silver", "Gold", "Diamond"][i] + "] HP: " + stat.lifebar + " / ATK: " + stat.attack, "description"));
                 i++;
             }
         }
@@ -170,17 +116,18 @@ function init(sa, ma, sig_only) {
     }
 }
 
-document.addEventListener("DOMContentLoaded", initialize);
+function newStringDiv(string, className) {
+    var div = document.createElement("div");
+    div.className = className;
+    div.innerHTML = string;
+    return div;
+}
 
+function formatDescription(feature, i) {
+    return format(corpus[feature.description], feature.tiers.slice(i)[0]);
+}
 
-
-
-
-
-
-
-
-function format(template, substitutions, id) {
+function format(template, substitutions) { // a bit hacky
     var matches = template.match(/{.*?}/g);
     var string = template;
     for (var match of matches) {
@@ -193,7 +140,51 @@ function format(template, substitutions, id) {
             substitute = Math.round(substitute);
             substitute += "%";
         }
-        string = string.replace(match, "<span id=\"" + id + "\">" + substitute + "</span>");
+        string = string.replace(match, "<span class=\"number\">" + substitute + "</span>");
     }
     return string;
 }
+
+function sort(method) {
+    var sorted = Object.keys(variants).sort(method);
+    for (var id of sorted) {
+        var card = document.getElementById(id);
+        collection.appendChild(card);
+    }
+    order = method;
+}
+
+function byAlpha(a, b) {
+    var varA = variants[a];
+    var varB = variants[b];
+    var A = fighters[varA.base].name + varA.name;
+    var B = fighters[varB.base].name + varB.name;
+    return A > B ? 1 : A < B ? -1 : 0;
+}
+function byHP(a, b) {
+    var varA = variants[a];
+    var varB = variants[b];
+    var A = varA.baseStats[varA.baseStats.length - 1];
+    var B = varB.baseStats[varB.baseStats.length - 1];
+    return B.lifebar - A.lifebar;
+}
+function byAttack(a, b) {
+    var varA = variants[a];
+    var varB = variants[b];
+    var A = varA.baseStats[varA.baseStats.length - 1];
+    var B = varB.baseStats[varB.baseStats.length - 1];
+    return B.attack - A.attack;
+}
+function byFS(a, b) {
+    var varA = variants[a];
+    var varB = variants[b];
+    var A = varA.baseStats[varA.baseStats.length - 1];
+    var B = varB.baseStats[varB.baseStats.length - 1];
+    return fightScore(B) - fightScore(A);
+}
+
+function fightScore(f) {
+    return (f.lifebar / 6 + f.attack) * 7 / 10;
+}
+
+document.addEventListener("DOMContentLoaded", initialize);
