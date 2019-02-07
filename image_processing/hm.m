6acb9c7c585bc17ea5aa88be919ad02a882a26ef
Ted Michael Celis <krazete@gmail.com>
2019-02-06 20:02:08 -0800

    completed raw_mask generator, edited masks in shadow_mask

--- image_processing/hm.m
+++ image_processing/hm.m
@@ -1,15 +1,51 @@
-art = dir('Art Capture');
-art_n = length(art);
-for i = 1:art_n
-    if ~startsWith(art(i).name, '.')
-        fighter = dir([art(i).folder, '/', art(i).name]);
-        fighter_n = length(fighter);
-        for j = 1:fighter_n
-            if ~fighter(j).isdir && length(regexp(fighter(j).name, '_(Card|PortraitMarquee)_')) < 1 && endsWith(fighter(j).name, '_B.png')
-                im = imread([fighter(j).folder, '/', fighter(j).name]);
+% Creates raw mask files.
+% The resulting raw_mask folder should be copied as shadow_mask.
+% All shadow_mask images should be cropped with the help of circle.png.
+% The shadow_mask folder should then be copied as color_mask.
+% All color_mask images should have the outermost black edges trimmed down.
+
+resetdir('raw_mask');
+
+fighters = dir('Art Capture');
+for i = 1:length(fighters)
+    clear('maxdelta');
+    isFolder = fighters(i).isdir;
+    isNested = ~startsWith(fighters(i).name, '.');
+    if isFolder && isNested
+        holder = {};
+        variants = dir([fighters(i).folder, '/', fighters(i).name]);
+        for j = 1:length(variants)
+            isPng = endsWith(variants(j).name, '.png');
+            isBasicPortrait = length(regexp(variants(j).name, '_(Card|Portrait)_')) < 1;
+            if isPng && isBasicPortrait
+                imName = [variants(j).folder, '/', variants(j).name];
+                [im, ~, alpha] = imread(imName);
+                holder{length(holder) + 1} = im;
                 imshow(im);
-                pause(0.01);
+                pause(0);
             end
         end
+        pairs = nchoosek(holder, 2);
+        for m = 1:length(pairs)
+            im1 = pairs{m, 1};
+            im2 = pairs{m, 2};
+            deltaR = abs(im2(:, :, 1) - im1(:, :, 1));
+            deltaG = abs(im2(:, :, 2) - im1(:, :, 2));
+            deltaB = abs(im2(:, :, 3) - im1(:, :, 3));
+            delta = deltaR + deltaG + deltaB;
+            if exist('maxdelta', 'var')
+                maxdelta = max(maxdelta, delta);
+            else
+                maxdelta = delta;
+            end
+        end
+        imwrite(maxdelta, ['raw_mask/', fighters(i).name, '.png'], 'Alpha', alpha);
     end
+end
+
+function resetdir(dirname)
+    if exist(dirname, 'dir')
+        rmdir(dirname, 's');
+    end
+    mkdir(dirname);
 end
