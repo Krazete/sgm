bfbcbf264e926519f525d92b735186dd5a584df9
Ted Michael Celis <krazete@gmail.com>
2023-01-11 20:47:28 -0800

    added bd to filters

--- fighter.js
+++ fighter.js
@@ -3,7 +3,7 @@ var userID, userIP, database;
 
 var tiers = ["bronze", "silver", "gold", "diamond"];
 var elements = ["neutral", "fire", "water", "wind", "dark", "light"];
-var fighterIDs = ["an", "be", "bb", "ce", "do", "el", "fi", "fu", "mf", "pw", "pa", "pe", "rf", "sq", "um", "va"];
+var fighterIDs = ["an", "be", "bb", "bd", "ce", "do", "el", "fi", "fu", "mf", "pw", "pa", "pe", "rf", "sq", "um", "va"];
 
 var cards = [];
 var filterCards;
