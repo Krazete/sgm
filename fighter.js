30833885debdc7763045a49b71a6c62dff572962
Ted Michael Celis <krazete@gmail.com>
2026-06-01 17:05:22 -0700

    8.7.0

--- fighter.js
+++ fighter.js
@@ -1752,18 +1752,13 @@ function initialize() {
             /* variant id search helper
             cards.forEach(e => e.innerHTML = e.id + "<br>" + corpus[variants[e.id].name]);
             */
-            var patchnotes = "https://hub.skullgirlsmobile.com/updates/game-update-86-patch-notes-a-memorable-picnic-after-the-journey";
+            var patchnotes = "https://hub.skullgirlsmobile.com/updates/game-update-87-patch-notes-an-inkling-of-whats-next-dye-hard";
             var vids = {
                 new: {
                     _url: patchnotes,
                     _class: "frame",
-                    mFlow: "Mana Flow Squigly,-Element",
-                    sTipper: "Scale Tipper Filia,-Element",
-                },
-                changed: {
-                    _url: patchnotes,
-                    _class: "sa",
-                    LLure: "Lucky Lure:",
+                    yOne: "Yellow One Squigly,-Element",
+                    iFluencer: "Influencer Peacock,-Element",
                 },
             };
 
